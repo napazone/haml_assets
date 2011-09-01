df8841684c2a7582433046d3706deb5ffab65c5d
Les Hill <leshill@gmail.com>
2011-09-01 09:25:00 -0700

    Do not register unless assets are enabled
    Closes #1

--- lib/haml_assets/engine.rb
+++ lib/haml_assets/engine.rb
@@ -1,6 +1,8 @@
 module HamlAssets
   class Engine < ::Rails::Engine
     initializer "sprockets.haml", after: "sprockets.environment" do |app|
+      next unless app.assets
+
       app.assets.register_engine('.haml', HamlSprocketsEngine)
     end
   end
