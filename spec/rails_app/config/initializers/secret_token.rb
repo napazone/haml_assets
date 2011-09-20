20c0b08c5e53909e80764700674f4669733827a6
Teng Siong Ong <siong1987@gmail.com>
2011-09-20 02:01:27 -0500

    Initial setup for rspec test.
    To create a test. Just add your .jst.ejs.haml template under spec/rails_app/app/assets/javascripts/.
    
    I have an initial test example up in spec/render_spec.rb.

---
+++ spec/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = 'e787a489c02bab15e4be9b57d8e5e9737667a04b839e6ccb06fe7a837808ed18baba96c7c18a39faaa33a3bd633211c7f2c79bbf4354a227cfa2ed4859fb3553'
