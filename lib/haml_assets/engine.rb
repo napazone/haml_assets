e264d6d9ebdafe4ee4573e35eedfd916ac04c6ca
Les Hill <leshill@gmail.com>
2012-01-25 00:24:48 -0800

    Register engine for :all group

--- lib/haml_assets/engine.rb
+++ lib/haml_assets/engine.rb
@@ -1,6 +1,6 @@
 module HamlAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.haml", :after => "sprockets.environment" do |app|
+    initializer "sprockets.haml", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
 
       app.assets.register_engine('.haml', HamlSprocketsEngine)
