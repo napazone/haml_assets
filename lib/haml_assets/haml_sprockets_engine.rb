5c1fc8f4d401453f437df37ba3826a9e03b63cd1
Les Hill <leshill@gmail.com>
2012-03-16 00:09:54 -0700

    Make the template buffers haml aware
    We no longer require a patched haml!

--- lib/haml_assets/haml_sprockets_engine.rb
+++ lib/haml_assets/haml_sprockets_engine.rb
@@ -10,11 +10,32 @@ module HamlAssets
     module ViewContext
       attr_accessor :output_buffer
 
+      def output_buffer_with_haml
+        return haml_buffer.buffer if is_haml?
+        output_buffer_without_haml
+      end
+
+      def set_output_buffer_with_haml(new)
+        if is_haml?
+          new = String.new(new) if Haml::Util.rails_xss_safe? &&
+            new.is_a?(Haml::Util.rails_safe_buffer_class)
+          haml_buffer.buffer = new
+        else
+          set_output_buffer_without_haml new
+        end
+      end
+
       def self.included(klass)
         klass.instance_eval do
           include Rails.application.routes.url_helpers
           include Rails.application.routes.mounted_helpers
           include ActionView::Helpers
+
+          alias_method :output_buffer_without_haml, :output_buffer
+          alias_method :output_buffer, :output_buffer_with_haml
+
+          alias_method :set_output_buffer_without_haml, :output_buffer=
+          alias_method :output_buffer=, :set_output_buffer_with_haml
         end
       end
 
