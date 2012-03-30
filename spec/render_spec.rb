79be2f436cea59a4793add7271100012cc4b52d8
Les Hill <leshill@gmail.com>
2012-03-30 13:46:48 -0700

    Quotes in attributes spec

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -4,4 +4,13 @@ describe HamlAssets do
   it "should have the proper format" do
     RailsApp::Application.assets['link_to.jst.ejs.haml'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"link_to\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('');}return __p.join('');};\n}).call(this);\n"
   end
+
+  context 'rendering' do
+    let(:path) { 'app/assets/templates/quotes.haml' }
+
+    it "quoted attributes" do
+      template = HamlAssets::HamlSprocketsEngine.new(path) { %Q(%div{data:{bind:'attr: { "data-something": someValue }'}}) }
+      template.send(:render_haml, Object.new, {}).strip.should eq(%Q(<div data-bind='attr: { "data-something": someValue }'></div>))
+    end
+  end
 end
