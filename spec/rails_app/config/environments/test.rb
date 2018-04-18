06705e4da47f19000c3110d3cf2ff902e804bdab
Eduardo Garibay <dark.cloud2007@gmail.com>
2018-04-18 16:23:13 -0500

    Silence deprecation warnings

--- spec/rails_app/config/environments/test.rb
+++ spec/rails_app/config/environments/test.rb
@@ -8,7 +8,7 @@ RailsApp::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
@@ -34,4 +34,6 @@ RailsApp::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  config.eager_load = false
 end
